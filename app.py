70125f8e318758d31ac8a91311f15aceebe26f24
Pratik-Gore262 <pratikgore0711@gmail.com>
2026-09-19 21:58:53 +0530

    added login authentication

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        role = request.form["role"]
+
+        if username == "admin" and password == "123":
+            return f"Welcome {role}"
+        return "Invalid Login"
+
     return render_template("login.html")
 
 if __name__ == "__main__":
